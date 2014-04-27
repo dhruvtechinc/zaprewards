d4cb6b4e4aa3971e924b68ac4b91b2219b5d097b
Sudheer K. Parasker <sparasker@gmail.com>
2014-04-26 20:25:02 -0500

    fixing issues after removing dist from repo

--- web.js
+++ web.js
@@ -11,5 +11,5 @@ app.use(bodyParser()); 						// pull information from html in POST
 app.use(methodOverride()); 					// simulate DELETE and PUT
 
 //app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(gzippo.staticGzip("" + __dirname + "/public"));
 app.listen(process.env.PORT || 5000);
